086388d12446cfc6872ed8d56ad2f9e3024825c0
Atom <trkien.dev@gmail.com>
2024-08-14 09:54:59 +0700

    Update missing_number.cs

--- problem_solving/Sorting/missing_number.cs
+++ problem_solving/Sorting/missing_number.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 
 class Program
 {
+    // * Solution 1: using List * //
     public static List<int> missingNumbers(List<int> arr, List<int> brr)
     {
         // Tạo một danh sách để lưu kết quả các số bị thiếu
@@ -33,6 +34,49 @@ class Program
         return missing;
     }
 
+    // ---------------------------------------------------------------------------------- //
+    
+    // * Solution 2: using Dictionary and Map * // 
+
+    public static List<int> missingNumbers2(List<int> arr, List<int> brr)
+    {
+        // Dictionary to store frequency of elements in brr
+        Dictionary<int, int> frequencyMap = new Dictionary<int, int>();
+    
+        // Populate the frequency map with elements from brr
+        foreach (int num in brr)
+        {
+            if (frequencyMap.ContainsKey(num))
+            {
+                frequencyMap[num]++;
+            }
+            else
+            {
+                frequencyMap[num] = 1;
+            }
+        }
+    
+        // Subtract the frequency of elements found in arr
+        foreach (int num in arr)
+        {
+            if (frequencyMap.ContainsKey(num))
+            {
+                frequencyMap[num]--;
+                // Remove the key if its frequency is zero
+                if (frequencyMap[num] == 0)
+                {
+                    frequencyMap.Remove(num);
+                }
+            }
+        }
+    
+        // Extract the keys (missing numbers) and sort them
+        List<int> missingNumbers = new List<int>(frequencyMap.Keys);
+        missingNumbers.Sort();
+    
+        return missingNumbers;
+    }
+    // ---------------------------------------------------------------------------------- //
     static void Main(string[] args)
     {
         List<int> arr = new List<int> { 3695, 3696, 3697, 3698 };
@@ -43,3 +87,8 @@ class Program
         Console.WriteLine("Missing numbers: " + string.Join(", ", result));
     }
 }
+
+
+
+
+
